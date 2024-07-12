39caf04b5c073126f573848a4cb576c83fc45343
Sukulsivakumar <sukulmestick@gmail.com>
2024-07-12 16:38:24 +0530

    12-07-2024 friday 04:38 PM

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <h1>
+          App.js
+        </h1>
+      </center>
+
+    </div>
+  )
+}
+
+export default App
